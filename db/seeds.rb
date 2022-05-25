9a839885b458f09e05b7384149b008ddf8b27853
shivani2001-art <shivshakannan821@gmail.com>
2022-05-25 11:13:37 +0530

    Finish user edit,update,index and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Create a main sample user.
+User.create!(name: "Shivani",
+    email: "shivani@gmail.com",
+    password: "shivani",
+    password_confirmation: "shivani",
+    admin: true)
+
+# Generate a bunch of additional users.
+99.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n+1}@gmail.com"
+    password = "password"
+    User.create!(name: name,
+                email: email,
+                password: password,
+                password_confirmation: password)
+end
